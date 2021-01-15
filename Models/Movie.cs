0113f78382dca2e610f337a098500bf8bf98d530
rodrigobentorodrigues <rodrigobentor2014@gmail.com>
2021-01-14 21:06:55 -0300

    Adicionado anotações a classe Movie

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,14 +9,21 @@ namespace Course.Models
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
+        [Display(Name = "Name:")]
         public string Name { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+        [Required]
+        [Display(Name = "Genre:")]
         public int GenreId { get; set; }
 
+        [Required]
+        [Display(Name = "Release Date:")]
         public DateTime ReleaseDate { get; set; }
+        [Required]
         public DateTime DateAdded { get; set; }
+        [Required]
+        [Display(Name = "Number in Stock:")]
         public int NumberInStock { get; set; }
 
     }
