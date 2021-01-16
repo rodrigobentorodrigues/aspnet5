a374fb9f9259ad95e23df8834b66b2f45ee94800
rodrigobentorodrigues <rodrigobentor2014@gmail.com>
2021-01-15 23:31:25 -0300

    Alterado formato de respostas da API

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System.Web.Http;
 
 namespace Course
@@ -9,6 +8,10 @@ namespace Course
     {
         public static void Register(HttpConfiguration config)
         {
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Formatting.Indented;
+
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
