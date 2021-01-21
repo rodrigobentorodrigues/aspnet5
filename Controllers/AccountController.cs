63485ef2e669221693aa6c4aa69046268714d107
rodrigobentorodrigues <rodrigobentor2014@gmail.com>
2021-01-21 16:43:26 -0300

    Add filter to Controllers with Identity

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Course.App_Start;
 using Course.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using System.Linq;
@@ -153,6 +154,12 @@ namespace Course.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    // TEMP
+                    var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    await roleManager.CreateAsync(new IdentityRole("CanManagerMovies"));
+                    await UserManager.AddToRoleAsync(user.Id, "CanManagerMovies");
+
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // Para obter mais informações sobre como habilitar a confirmação da conta e redefinição de senha, visite https://go.microsoft.com/fwlink/?LinkID=320771
